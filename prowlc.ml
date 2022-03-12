3d01b0291f362a3e75931c9f56c0062a1ee7c960
UberPyro <uberpyro181@gmail.com>
2022-03-12 12:21:46 -0500

    Add reasonable s-expression output for Asts

--- prowlc.ml
+++ prowlc.ml
@@ -1,26 +1,25 @@
 open Batteries
+open Sexplib
 (* open Sexplib *)
 module O = BatOptParse.Opt
 
 open Lib
 open Cli
+open Parse_proc
 
-let sexp_out = flag "output AST in s-expressions" "sexp"
 let lex_out = flag "output lexemes" "lex"
+let ast_out = flag "output ast as s-expression" "ast"
 
 let () = 
   match P.parse_argv op with
   | [] -> P.usage op ()
   | lst -> 
-    (* let is_sexp = O.get sexp_out in *)
     List.map begin fun file -> 
       (* let name, _ = Filename.split_extension file in *)
       if O.get lex_out then File.open_in file |> Gen.lex;
       let ast = File.open_in file |> Gen.ast in
-      (* if is_sexp then
-        ast
-        |> Ast.sexp_of_stmt
+      if O.get ast_out then
+        T.sexp_of_t ast
         |> Sexp.to_string_hum
-        |> print_endline *)
-        ignore ast
+        |> print_endline
     end lst |> ignore
--- src/gen/parse_proc.ml
+++ src/gen/parse_proc.ml
@@ -1,14 +1,16 @@
 open Batteries
-open Sexplib
+(* open Sexplib *)
 
 module M = struct
   open Lexing
-  let position_of_sexp _ = failwith "Cannot convert sexp to position"
+  (* let position_of_sexp _ = failwith "Cannot convert sexp to position"
   let sexp_of_position loc = Sexp.List [
     Sexp.Atom (string_of_int loc.pos_lnum); 
     Sexp.Atom (string_of_int loc.pos_cnum)
-  ]
-  type 'a m = 'a * (position * position)  [@@deriving sexp]
+  ] *)
+  let sexp_of_m f (a, _) = f a
+  let m_of_sexp _ = failwith "Cannot convert sexp into location"
+  type 'a m = 'a * (position * position)
 end
 
 module T = Ast.T(M)
