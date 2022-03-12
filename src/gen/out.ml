3dd3957a5117d699bf3b2992898f243eb28311cf
UberPyro <uberpyro181@gmail.com>
2022-03-12 11:32:41 -0500

    Work on out.ml

--- src/gen/out.ml
+++ src/gen/out.ml
@@ -17,4 +17,7 @@ and sp (x, _) = sp_t x
 and sp_t : T.sp_t -> O.sp_t = function
   | SPSp (x, y) -> SPSp (x, ty y)
   | SPTy (x, y) -> SPTy (x, ty y)
-  | SPAbst_ty  *)
+  | SPAbst_ty x -> SPAbst_ty x
+  | SPData (x, y) -> SPData (x, data y)
+
+and ty ((x, y), _) : T.ty -> O.ty = (List.map constr x, ty_body y) *)
