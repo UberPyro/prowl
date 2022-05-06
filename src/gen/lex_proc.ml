ff3b6e3aa771a0d04985ddeb5b84a914d5a34100
UberPyro <uberpyro181@gmail.com>
2022-05-06 03:16:27 -0400

    Initial commit

---
+++ src/gen/lex_proc.ml
@@ -0,0 +1,43 @@
+open Batteries
+open Lexing
+
+open Util
+
+let tabsize = ref 2
+
+type mode = Cat | Regex
+let mode = ref Cat
+
+let advance lexbuf = 
+  let lcp = lexbuf.lex_curr_p in
+  lexbuf.lex_curr_p <-
+    {lcp with pos_cnum = lcp.pos_cnum + !tabsize - 1}
+
+let set_cat = mode := Cat
+let set_regex = mode := Regex
+let is_cat a b = 
+  if !mode == Cat
+  then a else b
+
+let utf8encode s =
+  let prefs = [|0x0; 0xc0; 0xe0|] in
+  let s1 n = String.make 1 (Char.chr n) in
+
+  let rec ienc k sofar resid =
+    let bct = if k = 0 then 7 else 6 - k in
+    if resid < 1 lsl bct then (s1 (prefs.(k) + resid)) ^ sofar
+    else ienc (k + 1) (s1 (0x80 + resid mod 64) ^ sofar) (resid / 64) in
+  
+  ienc 0 "" (int_of_string ("0x" ^ s))
+
+let decode s =
+    let re = Str.regexp "\\\\u[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]" in
+    let subst = function
+      | Str.Delim u -> utf8encode (String.sub u 2 4)
+      | Str.Text t -> t in
+    String.concat "" (List.map subst (Str.full_split re s))
+
+let decode_char = 
+  decode
+  >> String.to_seq
+  >> Seq.hd
---
+++ src/util.ml
@@ -0,0 +1,49 @@
+let (>>) f g x = x |> f |> g
+let (>>>) f g x = let y, z = f x in g y z
+let (>>>>) f g x = let y, z, w = f x in g y z w
+
+let (||>) (x, y) f = f x y
+let (|||>) (x, y, z) f = f x y z
+
+let (<|) f x = f x
+let (<||) f (x, y) = f x y
+let (<|||) f (x, y, z) = f x y z
+
+let (<&>) d f = List.map f d
+let (&>) d v = d <&> fun _ -> v
+let (>>=) m f = m <&> f |> List.flatten
+
+let (let*) = (>>=)
+let (let+) = (<&>)
+
+let (<@>) d f = let* x = d in let+ g = f in g x
+let (@>) d v = d <&> fun _ -> v
+let (>>:) = (@>)
+let (let@) = (<@>)
+
+module Either = struct
+
+  type ('a, 'b) t = L of 'a | R of 'b
+
+  let (<&>) d f = match d with
+    | R r -> R (f r)
+    | e -> e
+  
+  let (>>=) m f = match m with
+    | R r -> f r
+    | e -> e
+  
+  let either e f g = match e with
+    | L l -> f l
+    | R r -> g r
+  
+  let bimap e f g = match e with
+    | L l -> L (f l)
+    | R r -> R (g r)
+  
+  let (let*) = (>>=)
+  let (let+) = (<&>)
+
+end
+
+type ('a, 'b) either = ('a, 'b) Either.t
