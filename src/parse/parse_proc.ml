e13297060c8046473f590b15636ab355c879a9c8
UberPyro <uberpyro181@gmail.com>
2022-05-20 14:32:58 -0400

    Unswitch lefts and rights

--- src/parse/parse_proc.ml
+++ src/parse/parse_proc.ml
@@ -4,8 +4,8 @@ let rec process_bin (p, l, r as con) = function
   | (_, [], _) -> failwith "Empty"
   | (0, h1 :: h2 :: t, 0) -> process_bin con (0, p h1 h2 :: t, 0)
   | (0, [x], 0) -> x
-  | (i, lst, 0) -> l (process_bin con (i - 1, lst, 0))
-  | (i, lst, j) -> r (process_bin con (i, lst, j - 1))
+  | (i, lst, 0) -> r (process_bin con (i - 1, lst, 0))
+  | (i, lst, j) -> l (process_bin con (i, lst, j - 1))
 
 let proc_ebin = process_bin (
   (fun (_, loc as e1) e2 -> Pair (e1, e2), loc), 
