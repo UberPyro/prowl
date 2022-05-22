0534754daf29d1bbf3132d9220feb92c4a9f5a1d
UberPyro <uberpyro181@gmail.com>
2022-05-21 20:07:24 -0400

    Handle extensions correctly in the lib loader

--- src/build.ml
+++ src/build.ml
@@ -9,8 +9,11 @@ type file =
 
 (* FIXME: File Extensions *)
 
+let is_dir = try Sys.is_directory with
+  | Failure _ -> Printf.sprintf "%s.prw" >> Sys.is_directory
+
 let rec load_file ?(path="") fn = 
-  if Sys.is_directory (path ^ fn)
+  if is_dir (path ^ fn)
   then
     Sys.readdir (path ^ fn)
     |> Array.map (load_file ~path:(path ^ fn ^ "/"))
@@ -20,7 +23,7 @@ let rec load_file ?(path="") fn =
 
 let rec def_of_file = function
   | File (path, fn) ->
-    File.open_in (path ^ fn)
+    File.open_in (path ^ fn ^ ".prw")
     |> Gen.parse
     |> fun (am, (_, loc as e1)) -> 
     Def (am, false, (PId fn, loc), e1, None)
@@ -29,8 +32,6 @@ let rec def_of_file = function
     |> List.map (fun fn -> def_of_file fn, dum)
     |> fun def_lst -> 
     Def (Pub, false, (PId sn, dum), (Mod def_lst, dum), None)
-
-let wrap_stmt s_t = Mod [s_t, dum], dum
 
 let endow lib (am, e) = 
   lib
