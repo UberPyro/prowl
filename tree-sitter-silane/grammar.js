3d485fbc9168406079af99bb64e4a2b112ee0ef3
UberPyro <uberpyro181@gmail.com>
2022-07-11 14:34:20 -0400

    Add basic infrastructure

--- tree-sitter-silane/grammar.js
+++ tree-sitter-silane/grammar.js
@@ -1,7 +1,102 @@
+const id_tail = "(\-?[A-Za-z0-9_\'])*";
+const sym = "[~!@#$%^&*\\-=+\\.?:<>|/\\\\]+";
+const suffix = "[?+*!]|!!";
+const comment = /([^*\/]|\*[^\/]|\/[^*]|[ \t\n])*[^*\/]/;
+
 module.exports = grammar({
   name: 'silane',
 
+  extras: $ => [
+    /\s/, 
+    $.comment, 
+  ], 
+
+  word: $ => $.id, 
+
   rules: {
-    source_file: $ => 'yo.'
+    source_file: $ => 'yo.',
+
+    quant: $ => token.immediate(/[?+*][?+]?/),
+    check: $ => token.immediate(/!!?/),
+    id: $ => new RegExp("[a-z]" + id_tail),
+    cap: $ => new RegExp("[A-Z]" + id_tail),
+    exn: $ => new RegExp("[A-Z]" + id_tail + "\\!"),
+    err: $ => new RegExp("[A-Z]" + id_tail + "\\!\\!"),
+    _exn: $ => choice($.exn, $.err),
+
+    symbol: $ => new RegExp(sym), 
+    blank: $ => new RegExp("_" + id_tail),
+    int: $ => /0|[1-9][0-9]*/, 
+    float: $ => /[1-9]\.[0-9]*|\.[0-9]+/, 
+    string: $ => seq('"', repeat($._string_content), '"'),
+    char: $ => seq("'", optional($._character_content), "'"),
+
+    _string_content: $ => choice(
+      token.immediate(' '),
+      token.immediate('\n'),
+      token.immediate('\t'),
+      /[^\\"]+/,
+      $.escape_sequence
+    ),
+
+    _character_content: $ => choice(
+      /[^\\']/,
+      $.escape_sequence
+    ),
+
+    escape_sequence: $ => choice(
+      /\\[\\"'ntbr ]/,
+      /\\[0-9][0-9][0-9]/,
+      /\\x[0-9A-Fa-f][0-9A-Fa-f]/,
+      /\\o[0-3][0-7][0-7]/
+    ),
+
+    comment: $ => seq(
+      "/*", choice(
+        comment, 
+        comment, $.comment, comment
+      ), "*/"
+    ),
+
+
+    // Type Language
+    _bound: $ => choice($.int, $.id),
+    _span: $ => choice(
+      seq("(", optional($._bound), ":", optional($._bound), ")"),
+      seq("(", optional($._bound), ")"),
+      "erroneous", "det", "multi", 
+      "failure", "semidet", "nondet",
+    ),
+
+    _param_tail: $ => choice($.int, "."),
+    param_stack: $ => seq($._param_tail, repeat($.cap)),
+    _param: $ => choice(
+      $.cap, 
+      seq("[", $.param_stack, "--", $.param_stack, "]"),
+      $._span,
+    ),
+
+    _name: $ => choice(
+      $.id, 
+      seq("(", $.symbol, ")"), 
+      seq("{", $.symbol, "}"),
+      seq("{", "and", token.immediate($.symbol), "}"),
+    ),
+
+    // Type Language
+
   }
 });
+
+function sep(delimiter, rule) {
+  return optional(sep1(delimiter, rule))
+}
+
+function sep1(delimiter, rule) {
+  return seq(
+    optional(delimiter), 
+    rule, 
+    repeat(seq(delimiter, rule)), 
+    optional(delimiter)
+  )
+}
