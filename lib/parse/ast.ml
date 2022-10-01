644160dc5f3682f11d6ea0bbbf181879c8a42082
UberPyro <uberpyro181@gmail.com>
2022-10-01 06:09:13 -0400

    Update ast

--- lib/parse/ast.ml
+++ lib/parse/ast.ml
@@ -1,9 +1,23 @@
-type e = w list list
-and w = 
+open Batteries
+open Lexing
+
+type loc = position * position
+
+type 'a wrap = <
+  ast : 'a; 
+>
+
+type e = e wrap
+and _e = w list list
+and w = _w wrap
+and _w = 
   | IntLit of int
   | CharLit of char
   | QuoteLit of e
   | ListLit of e list
   | Id of string
+
+type s = s_ wrap
+and s_ = string * e
 
 type p = (string * e) list
