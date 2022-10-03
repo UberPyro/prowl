37e696b2fb10b88d888e8b508c1d6c309ade1ed1
UberPyro <uberpyro181@gmail.com>
2022-10-03 16:52:58 -0400

    First arm working

--- lib/type/check.ml
+++ lib/type/check.ml
@@ -15,8 +15,8 @@ type 'a typed_content = <
   ty : t * t;
 >
 
-let ascribe f ty obj = object
-  method ast = f obj#ast
+let ascribe x ty obj = object
+  method ast = x
   method loc = obj#loc
   method ty = ty
 end
@@ -59,14 +59,12 @@ let new_lit v =
   let s2 = push_var v s1 in
   T (push_stack s1 c1), T (push_stack s2 c1)
 
-let ascribe_lit lit = ascribe Fun.id (new_lit lit)
-
 let rec type_expr c e = 
   ascribe (List.map (type_word c)) (new_endo ()) e
 
 and type_word (c : (Type.t * Type.t) Dict.t) (w : ContentAst.w) : TypedAst.w = 
   match w#ast with
-  | IntLit _ -> ascribe_lit (uref Var.(Mono Int)) w
+  | IntLit i -> ascribe (TypedAst.IntLit i) (new_lit (uref Var.(Mono Int))) w
   (* | CharLit _ -> ascribe_lit (uref Var.(Mono Char)) w *)
   (* | Id s -> ascribe Fun.id (Dict.find s c) w *)
   (* | QuoteLit e -> 
@@ -79,4 +77,3 @@ and type_word (c : (Type.t * Type.t) Dict.t) (w : ContentAst.w) : TypedAst.w =
 (* and type_word = 
   let open TypedAst in
   | ContentAst.IntLit *)
-let z (y : ContentAst.w) = ascribe_lit (uref Var.(Mono Int)) y
