28b2c2511619c706db6baf0ae12309759ff2e7de
UberPyro <uberpyro181@gmail.com>
2022-10-03 12:39:58 -0400

    Write helpers for typechecking

--- lib/type/check.ml
+++ lib/type/check.ml
@@ -15,3 +15,35 @@ let pp_typed_content = pp_content
 module TypedAst = Wrap(struct
   type 'a t = 'a typed_content [@@deriving show]
 end)
+
+let new_costack () = 
+  uref @@ Costack.Bot (Costack.fresh ())
+
+let new_stack () = 
+  uref @@ Stack.Bot (Stack.fresh ())
+
+let push_stack s c = 
+  uref @@ Costack.Seq (s, c)
+
+let push_var v s = 
+  uref @@ Stack.Seq (v, s)
+
+let new_distack () = 
+  push_stack (new_stack ()) (new_costack ())
+
+let new_fn () = 
+  new_costack (), new_costack ()
+
+let new_endo () = 
+  let c = new_costack () in
+  c, c
+
+let new_stack_endo () = 
+  let c = new_distack () in
+  c, c
+
+let new_lit v = 
+  let c1 = new_costack () in
+  let s1 = new_stack () in
+  let s2 = push_var v s1 in
+  push_stack s1 c1, push_stack s2 c1
