01ef4c2701a4c98ff877603e8ed0a5301bffe7bd
UberPyro <uberpyro181@gmail.com>
2022-10-04 11:11:12 -0400

    Add flag processing

--- bin/main.ml
+++ bin/main.ml
@@ -6,11 +6,21 @@ open Cli
 open Ast
 open Gen
 
+let ast_out = flag "dump ast" "ast"
+let typecheck = flags "typecheck program" "check" 't'
+
+let process file = 
+  let ast = File.open_in file |> parse in
+  begin if O.get ast_out then
+    let str = ContentAst.show_p ast in
+    String.nreplace ~str ~sub:"Ast.Wrap." ~by:""
+    |> print_endline
+  end
+  (* begin if O.get typecheck then
+    ast |> Check.type_expr
+  end *)
+
 let () = match P.parse_argv op with
   | [] -> P.usage op ()
-  | file :: _ -> 
-    File.open_in file
-    |> parse
-    |> ContentAst.show_p
-    |> (fun str -> String.nreplace ~str ~sub:"Ast.Wrap." ~by:"")
-    |> print_endline
+  | lst :: _ -> 
+    process lst
