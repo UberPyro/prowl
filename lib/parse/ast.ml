217bbe36e8dd35f3e061a3fa0c9cfa3017952466
UberPyro <uberpyro181@gmail.com>
2022-11-24 10:35:14 -0500

    Finish ast for types

--- lib/parse/ast.ml
+++ lib/parse/ast.ml
@@ -11,6 +11,7 @@ and spec =
   | SMix of sign
   | SKind of int * string
   | STy of param list * string * vl
+  | SData of param list * string * data
   | SSpec of string * ty
   | SSign of string * sign
   | SModl of (string * sign) list * string * sign
@@ -35,6 +36,7 @@ and defn =
   | DMix of modl
   | DKind of int * string
   | DTy of param list * string * vl
+  | DData of param list * string * data
   | DDef of pat list * string * ty option * expr
   | DSign of string * sign
   | DModl of (string * sign) list * string * sign option * modl
@@ -42,9 +44,26 @@ and defn =
 
 
 (* Type *)
-and ty
+and ty = costack * costack
 
-and vl
+and costack = 
+  | CImpl of stack list
+  | CExpl of int * stack list
+
+and stack = 
+  | KImpl of vl list
+  | KExpl of string * vl list
+
+and vl = 
+  | VId of string
+  | VDir of dir * vl
+  | VGen of string
+  | VFun of ty
+  | VList of ty
+
+and data = 
+  | Record of (string * ty) list
+  | Variant of (vl list * string) list
 
 
 (* Expression *)
