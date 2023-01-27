3c51ef5b7fc60ba266483c5300848eee8294e511
UberPyro <uberpyro181@gmail.com>
2023-01-27 17:23:54 -0500

    Add seq bottoms

--- lib/type/ulist.ml
+++ lib/type/ulist.ml
@@ -3,25 +3,31 @@ open Uref
 
 let pp_uref f z y = f z (uget y)
 
+exception Err
+
 type ('a, 'b) t = ('a, 'b) _t uref
 and ('a, 'b) _t = 
   | Push of ('a, 'b) t * 'a
-  | Empty of 'b [@@deriving show]
+  | Next of 'b
+  | Bot [@@deriving show]
 
 let rec unite_seq ~sel r = 
   r |> unite ~sel:begin curry @@ function
-    | Empty _ as s, Empty _ -> s
-    | Push _ as s, Empty _ | Empty _, (Push _ as s) -> s
+    | Next _ as s, Next _ -> s
+    | Push _ as s, Next _ | Next _, (Push _ as s) -> s
     | Push (a, t) as s, Push (b, u) -> 
       sel t u;
       unite_seq ~sel a b;
       s
+    | Push _, Bot | Bot, Push _ -> raise Err
+    | Next _, Bot | Bot, Next _ -> Bot
+    | Bot, Bot -> Bot
   end
 
 let make init = 
   List.fold_left begin fun ulst x -> 
     uref @@ Push (ulst, x) 
-  end @@ uref @@ Empty init
+  end @@ uref @@ Next init
 
 let push us u = uref @@ Push (us, u)
 
@@ -29,12 +35,15 @@ let rec to_list ulst = match uget ulst with
   | Push (us, u) -> 
     let h, x = to_list us in
     u :: h, x
-  | Empty x -> [], x
+  | Next x -> [], Some x
+  | Bot -> [], None
 
 let rec iter f g ulst = match uget ulst with
   | Push (us, u) -> f u; iter f g us
-  | Empty x -> g x
+  | Next x -> g x
+  | Bot -> ()
 
 let rec remap f g ulst = uref @@ match uget ulst with
   | Push (us, u) -> Push (remap f g us, f u)
-  | Empty x -> Empty (g x)
+  | Next x -> Next (g x)
+  | Bot -> Bot
--- lib/type/ulist.mli
+++ lib/type/ulist.mli
@@ -6,8 +6,8 @@ type ('a, 'b) t
 val unite_seq : sel:('a -> 'a -> unit) -> ('a, 'b) t -> ('a, 'b) t -> unit
 val make : 'a -> 'b list -> ('b, 'a) t
 val push : ('a, 'b) t -> 'a -> ('a, 'b) t
-val to_list : ('a, 'b) t -> 'a list * 'b
-val iter : ('a -> unit) -> ('b -> 'c) -> ('a, 'b) t -> 'c
+val to_list : ('a, 'b) t -> 'a list * 'b option
+val iter : ('a -> unit) -> ('b -> unit) -> ('a, 'b) t -> unit
 val remap : ('a -> 'b) -> ('c -> 'd) -> ('a, 'c) t -> ('b, 'd) t
 
 val show : (Format.formatter -> 'a -> unit) -> (Format.formatter -> 'b -> unit) -> ('a, 'b) t -> string
