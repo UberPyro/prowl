ca6d36e0ddcb5a119c190a17ec4fe23eb5034a67
UberPyro <uberpyro181@gmail.com>
2023-02-26 11:47:33 -0500

    Add an improved unifiable lists implementation

---
+++ lib/util/ulist.ml
@@ -0,0 +1,46 @@
+open! Batteries
+open Uref
+
+module Uref = struct
+  include Uref
+  let pp fmt x y = fmt x (uget y)
+end
+
+type 'a t = 'a _t Uref.t
+and 'a _t = 
+  | UCons of 'a * 'a t
+  | UNil
+  | USeq of 'a [@@deriving show]
+
+let unil () = uref UNil
+let ucons u us = uref @@ UCons (u, us)
+let useq u = uref @@ USeq u
+
+let ufront us = match uget us with
+  | UCons (u, us) -> Either.Right (u, us)
+  | USeq u -> Either.Left (Some u)
+  | UNil -> Either.Left None
+
+let rec umap ?(g=Fun.id) f us = uref @@ match uget us with
+  | UCons (u, us) -> UCons (f u, umap ~g f us)
+  | USeq u -> USeq (g u)
+  | UNil -> UNil
+
+let copy us = umap Fun.id us
+
+let rec uiter ?(g=ignore) f us = match uget us with
+  | UCons (u, us) -> f u; uiter ~g f us
+  | USeq u -> g u
+  | UNil -> ()
+
+let rec unite unite_val r = 
+  r |> Uref.unite ~sel:begin fun s t -> match s, t with
+    | USeq _, USeq _ -> s
+    | (UCons _ as x), USeq _ | USeq _, (UCons _ as x) -> x
+    | UCons (u, us), UCons (v, vs) -> 
+      unite_val u v;
+      unite unite_val us vs;
+      s
+    | UCons _, UNil | UNil, UCons _ -> raise @@ Invalid_argument "unite"
+    | USeq _, UNil | UNil, USeq _ | UNil, UNil -> UNil
+  end
---
+++ lib/util/ulist.mli
@@ -0,0 +1,14 @@
+open! Batteries
+
+type 'a t [@@deriving show]
+
+val unil : unit -> 'a t
+val ucons : 'a -> 'a t -> 'a t
+val useq : 'a -> 'a t
+val ufront : 'a t -> ('a option, 'a * 'a t) Either.t
+
+val umap : ?g:('a -> 'a) -> ('a -> 'a) -> 'a t -> 'a t
+val copy : 'a t -> 'a t
+val uiter : ?g:('a -> unit) -> ('a -> unit) -> 'a t -> unit
+
+val unite : ('a -> 'a -> unit) -> 'a t -> 'a t -> unit
