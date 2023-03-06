66fc81dc9c8765188beb2600144908dab705e5ce
UberPyro <uberpyro181@gmail.com>
2023-03-05 22:15:40 -0500

    Add spanned errors to kind spec derivation

---
+++ lib/infer/error.ml
@@ -0,0 +1,5 @@
+open! Batteries
+
+open Meta
+
+exception TypeError of Span.t * string
--- lib/infer/kind_spec.ml
+++ lib/infer/kind_spec.ml
@@ -1,9 +1,10 @@
 open! Batteries
 
 open Syntax
+open Meta
 open Type
 
-let rec derive ((k_, sp) : Ast.kind) : Kind.t = match k_ with
+let rec derive ((k_, _) : Ast.kind) : Kind.t = match k_ with
   | `var -> Kind.lit Kind.KStar
   | `seq -> Kind.start ()
   | `jux [] -> Kind.connected ()
@@ -11,7 +12,12 @@ let rec derive ((k_, sp) : Ast.kind) : Kind.t = match k_ with
     let i = derive h, snd h in
     let rec connect_pairs (h_, sp_h as h) = function
       | (i_, sp_i as i) :: t -> 
-        Kind.connect h_ i_;
+        begin try Kind.connect h_ i_ with
+        | Kind.DifferentlyKinded (s1, s2) -> raise @@ Error.TypeError (
+            Span.join sp_h sp_i, 
+            Printf.sprintf "Cannot unify kinds [%s] and [%s]" s1 s2
+          )
+        end;
         connect_pairs i t
       | [] -> h in
     let o = connect_pairs i (List.map (fun x -> derive x, snd x) t) in
