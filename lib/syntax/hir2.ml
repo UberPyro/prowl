023ef60718e37519af96f433172366707e58935a
UberPyro <uberpyro181@gmail.com>
2023-03-17 06:59:56 -0400

    Scan for unbound uvars

--- lib/syntax/hir2.ml
+++ lib/syntax/hir2.ml
@@ -25,7 +25,23 @@ module Ins = struct
 
   let ins v e : Hir1.expr = `bind_uvar ([v], e), snd e
 
-  (* let scan *)
+  (* Can't be used as-is -- has to be woven into `expr`. *)
+  let scan = 
+    let rec go vc_unbound vc_bound ((e_, sp) : Hir1.expr) = match e_ with
+      | #Ast.literal | #Hir1.comb | #Ast.lexical_variable -> vc_unbound, vc_bound
+      | `var v | `stack v | `costack v -> 
+        begin vc_unbound |> if Vocab.mem v vc_bound then Fun.id
+        else Vocab.add v sp end, vc_bound
+      | `jux es -> List.fold_left (fun (u, b) -> go u b) (vc_unbound, vc_bound) es
+      | `dag e -> go vc_unbound vc_bound e
+      | `prime e -> go vc_unbound vc_bound e
+      
+      | `quote _ | `block _ -> vc_unbound, vc_bound
+
+      | `bind_var (_, e) -> go vc_unbound vc_bound e
+      | `bind_uvar (vs, e) -> 
+        go vc_unbound (List.fold_left (fun vc v -> Vocab.add v sp vc) vc_bound vs) e
+    in go Vocab.empty Vocab.empty %> fst
 
   (* let rec expr vc ((e_, sp) : Hir1.expr) : Hir1.expr = begin match e_ with
     | 
