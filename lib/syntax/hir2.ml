1fc0909caaf580fed8734cb34eb2aa7926b282c9
UberPyro <uberpyro181@gmail.com>
2023-03-22 07:24:35 -0400

    Start binder rewrite

--- lib/syntax/hir2.ml
+++ lib/syntax/hir2.ml
@@ -21,6 +21,52 @@ and _expr = [
 
 module Vocab = Map.Make(Var)
 
+(* rewrite for better automatic binder insertion *)
+module Ins2 = struct
+
+  module Vocab = Set.Make(Var)
+
+  type t = {
+    unbound : Vocab.t; 
+    unbound_converse : Vocab.t;
+    bound : Vocab.t;
+    expr : Hir1.expr;
+  }
+
+  let (let+) t f = {t with expr = Tuple2.map1 f t.expr}
+
+  let rec expr_outer (r : t) : t = 
+    let r' = expr_inner r in
+    let+ _ = r in `bind_uvar (Vocab.elements r'.unbound, r'.expr)
+  
+  and expr_inner (r : t) : t = match fst r.expr with
+    | #Ast.literal | #Hir1.comb | #Ast.lexical_variable -> r
+    | `var _ | `stack _ | `costack _ as e_ -> expr_collect r e_ `direct
+    | `dag ((`var _ | `stack _ | `costack _ as e_), _) -> 
+      expr_collect r e_ `converse
+    | `jux es -> 
+      let r', es' = List.fold_left begin fun (r', es') e' -> 
+        let y = expr_inner (let+ _ = r' in fst e') in
+        y, y.expr :: es'
+      end (r, []) es in 
+      let+ _ = r' in `jux (List.rev es')
+    | `prime e' -> 
+      let+ x = expr_inner (let+ _ = r in fst e') in `prime (x, snd e')
+    
+
+    | _ -> failwith "Todo"
+
+  
+  and expr_collect r (`var v | `stack v | `costack v) = 
+    let col = 
+      if Vocab.mem v r.bound then Fun.id
+      else Vocab.add v in function
+    | `direct -> {r with unbound = col r.unbound}
+    | `converse -> {r with unbound_converse = col r.unbound_converse}
+
+
+end
+
 module Ins = struct
 
   type t = Span.t Vocab.t * Span.t Vocab.t * Hir1.expr
@@ -49,7 +95,7 @@ module Ins = struct
       let bs' = List.fold_left begin fun (u, b, bs') (s, e') -> 
         let+ x = expr_outer (u, b, e') in (s, x) :: bs'
       end (unbound, bound, []) bs |> Tuple3.third |> List.rev in
-      let+ x = expr_inner (unbound, bound, e_) in (`bind_var (bs', x), sp)
+      let+ x = expr_inner (unbound, bound, e_) in `bind_var (bs', x), sp
     | `bind_uvar (vs, e') -> 
       let b = List.fold_left (fun vc v -> Vocab.add v sp vc) bound vs in
       let+ x = expr_inner (unbound, b, e') in `bind_uvar (vs, x), sp
