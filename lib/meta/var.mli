291d08df09dc84df99d8f6a7358d87c877769720
UberPyro <uberpyro181@gmail.com>
2023-03-25 18:28:19 -0400

    initial commit

--- lib/meta/var.mli
+++
@@ -1,8 +0,0 @@
-type t [@@deriving show, eq, ord]
-
-exception OccursError of string
-
-val name : string -> t
-val num : int -> t
-val fresh : unit -> t
-val refresher : unit -> < freshen : t -> t >
--- lib/syntax/ast.ml
+++ lib/syntax/ast.ml
@@ -2,23 +2,6 @@ open! Batteries
 
 open Meta
 
-type monodet = {total : bool ; bare : bool}
-
-type unification_variable = [
-  | `var of Var.t
-  | `stack of Var.t
-  | `costack of Var.t
-] [@@deriving show]
-
-type lexical_variable = [
-  | `id of string
-] [@@deriving show]
-
-type variable = [
-  | unification_variable 
-  | lexical_variable
-] [@@deriving show]
-
 type literal = [
   | `int of int
   | `float of float
@@ -28,19 +11,18 @@ type literal = [
 
 type expr = _expr * Span.t [@@deriving show]
 and _expr = [
-  | variable
   | literal
+  | `var of string
+  | `id of string
 
   | `jux of expr list
   | `dag of expr
-  | `prime of expr
 
   | `quote of expr
   | `list of expr list
-  | `block of expr * monodet * monodet
 
   | `bind_var of (string * expr) list * expr
-  | `bind_uvar of Var.t list * expr
+  | `bind_uvar of string list * expr
 
   | `unop of expr * string
   | `binop of expr * string * expr
