365b696210287a2cd63b5589f8bbaea7b803b17c
UberPyro <uberpyro181@gmail.com>
2023-03-27 12:23:13 -0400

    Implement eval for combinators

--- lib/run/eval_hir1.ml
+++ lib/run/eval_hir1.ml
@@ -5,19 +5,14 @@ open Uref
 open Syntax
 (* open Meta *)
 
-exception Kablooey of string
-exception DifferentlyHeighted
-exception DifferentlyExtant
-exception StuntedCostack
-exception EmptyStack
+type common = [
+  | `int of int
+  | `string of string
+  | `quote of Hir1.expr
+  | `list of Hir1.expr list
+] [@@deriving show]
 
-type _value = 
-  | Int of int
-  | String of string
-  | Quote of Hir1.expr
-  | List of Hir1.expr list
-  | Poly
-  [@@deriving show]
+type _value = [ common | `poly ] [@@deriving show]
 
 module Sol = Set.Make(struct
   type t = _value
@@ -39,16 +34,26 @@ and _ucostack =
   | Over of ucostack
   | Void
 
+exception Kablooey of string
+exception DifferentlyHeighted
+exception DifferentlyExtant
+exception StuntedCostack
+exception EmptyStack
+exception DifferentlyTyped of _value * _value
+exception Noncallable of _value
+
+let mk_val s = uref @@ Sol.singleton s
+
 let select_val v1 v2 = match v1, v2 with
-  | Int i1, Int i2 -> 
+  | `int i1, `int i2 -> 
     if i1 = i2 then Sol.singleton v1
     else Sol.empty
-  | String s1, String s2 -> 
+  | `string s1, `string s2 -> 
     if s1 = s2 then Sol.singleton v1
     else Sol.empty
-  | Quote _, Quote _ -> raise @@ Kablooey "quote"
-  | List _, List _ -> raise @@ Kablooey "list"
-  | _, _ -> Sol.empty
+  | `quote _, `quote _ -> raise @@ Kablooey "quote"
+  | `list _, `list _ -> raise @@ Kablooey "list"
+  | _v1, _v2 -> raise @@ DifferentlyTyped (_v1, _v2)
 
 let unify_val = unite ~sel:begin fun x y -> 
   List.cartesian_product (Sol.to_list x) (Sol.to_list y)
@@ -95,7 +100,7 @@ let (let$) (x, y) f =
 
 let pop = uget %> function
   | Push (u, v) -> u, v
-  | Next u -> u, uref @@ Sol.singleton Poly
+  | Next u -> u, mk_val `poly
   | Unit -> raise EmptyStack
 
 let pop2 u0 = 
@@ -107,8 +112,10 @@ let push u v = uref @@ Push (u, v)
 let push2 u v2 v1 = push (push u v2) v1
 
 let map_val f v = uref @@ Sol.map f (uget v)
+(* let fold_val f acc v = Sol.fold f acc (uget v) *)
+let iter_val f v = Sol.iter f (uget v)
 
-let expr ((e_, sp) : Hir1.expr) i o = match e_ with
+let rec expr ((e_, sp) : Hir1.expr) i o = match e_ with
   | `gen -> unify_costack i o
   | `fab -> unify_costack (uref @@ Fake i) o
   | `swap -> 
@@ -119,14 +126,27 @@ let expr ((e_, sp) : Hir1.expr) i o = match e_ with
     let$ (i, o) = i, o in
     let u, v = pop i in
     let v' = v |> map_val @@ fun x -> 
-      Quote begin match x with
-        | Int i -> `int i, sp
-        | String s -> `string s, sp
-        | Quote e -> `quote e, sp
-        | List es -> `list es, sp
-        | Poly -> `dag (`zap, sp), sp
+      `quote begin match x with
+        | #common as c -> c, sp
+        | `poly -> `dag (`zap, sp), sp
       end in
     unify_stack (push u v') o
+  | `call -> 
+    let$ (i', _) = i, o in
+    let u, v = pop i' in
+    v |> iter_val begin function
+      | `quote e -> expr e (uref @@ Real u) o
+      | _v -> raise @@ Noncallable _v
+    end
+  | `zap -> 
+    let$ (i, o) = i, o in
+    let u, _ = pop i in
+    unify_stack u o
+  
+  | #common as c -> 
+    let$ (i, o) = i, o in
+    unify_stack (push i @@ mk_val c) o
+
   
   
   | _ -> failwith "todo"
