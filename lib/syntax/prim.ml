5a3456076ae597acd6953e10dbd7b27529f62e48
UberPyro <uberpyro181@gmail.com>
2023-04-02 13:24:51 -0400

    Add quantifiers to mir

--- lib/syntax/prim.ml
+++ lib/syntax/prim.ml
@@ -11,7 +11,7 @@ type word = [
 type op = [
   | `gen | `fab | `elim | `exch
   | `swap | `unit | `call | `zap | `dup
-  | `cat | `dis
+  | `cat | `dis | `star | `mark
 
   | `eq | `neq | `lt | `gt | `le | `ge | `not
   | `add | `sub | `mul | `div | `rem | `neg
