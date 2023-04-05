3084bdd2fca022b769dc1cace659e92add369899
UberPyro <uberpyro181@gmail.com>
2023-04-04 21:04:26 -0400

    Sketch for fac

--- bin/main.ml
+++ bin/main.ml
@@ -16,9 +16,16 @@ let () = print_newline ()
 let jx x = `jux (List.map (fun x -> x, sp) x)
 (* let dg x = `dag (x, sp)
 let ds x y = `dis ((x, sp), (y, sp)) *)
-let pk x = `pick (List.map (fun x -> x, sp) x)
+(* let pk x = `pick (List.map (fun x -> x, sp) x) *)
 
 let () = LazyList.iter (Eval_mir.show_costack %> print_endline) @@ 
   Eval_mir.expr Eval_mir.init (jx [
-    `int 0; `int 5; `int 4; `cmp; pk [`succ; `jux []; `pred]
+    `int 7; `int 1; 
+    `star (jx [
+      `quote (jx [
+        `dup; `int 0; `eq; `exch; `pred; `dup
+      ], sp);
+      `dip; `mul; `elim;
+    ], sp);
+    `swap; `zap;
   ], sp) (Real [])
