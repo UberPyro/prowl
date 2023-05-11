da627a728ea57b624ecc5f2e686cd4e132b0f124
UberPyro <uberpyro181@gmail.com>
2023-05-11 18:00:20 -0400

    Initial commit

---
+++ bin/main.ml
@@ -0,0 +1,4 @@
+let () = print_endline "
+A journey of a thousand miles begins with a single step. 
+(todo: something more insightful than a quote)
+"
---
+++ lib/parse/ast.ml
@@ -0,0 +1,67 @@
+open! Batteries
+
+module type T = sig
+  type t [@@deriving show]
+end
+
+module Make(M : T) = struct
+
+  type expr = _expr * M.t
+  and _expr = 
+    | Bop of expr * bop * expr
+    | SectLeft of bop * expr
+    | SectRight of expr * bop
+    | Sect of bop
+    | Uop of expr * uop
+    | Dop of expr * dop * expr
+    | Nop of nop
+
+    | Var of string
+    | UVar of string
+    | Let of stmt list * expr
+    | Ex of string list * expr
+
+  (* binary operators *)
+  and bop = _bop * M.t
+  and _bop = 
+    | Add
+    | Sub
+    | Mul
+    | DivMod
+
+    | Eq
+    | Neq
+    | Gt
+    | Lt
+    | Ge
+    | Ne
+  
+  (* unary (dataflow) operators *)
+  and uop = _uop * M.t
+  and _uop = 
+    | Dag
+  
+  (* dataflow operators *)
+  and dop = _dop * M.t
+  and _dop = 
+    | Tensor
+    | Ponder
+    | Fork
+    | Pick
+    
+    | Jux
+    | Union
+  
+  (* nullary/stack operators *)
+  and nop = _nop * M.t
+  and _nop = 
+    | Gen | Fab | Exch | Elim
+    | Dup | Zap | Swap | Cons | Dip | Cat | Unit
+    | Lin | Parse | Show
+  
+  and stmt = _stmt * M.t
+  and _stmt = 
+    | Def of string * expr
+    [@@deriving show]
+
+end
