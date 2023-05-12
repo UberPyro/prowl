f01e11cf44fd0a6f06e3ac97c41d05363311dd05
UberPyro <uberpyro181@gmail.com>
2023-05-11 20:24:59 -0400

    Remove module wrapper

--- lib/check/abel_list.ml
+++ lib/check/abel_list.ml
@@ -5,42 +5,30 @@ open Uref
 
 exception AbelError
 
-module type Abelian = sig
-  type t [@@deriving show, eq]
+type uabel = _uabel uref
+and _uabel = 
+  | Abel of int
+  | AVar of int
 
-  val add : t -> t -> t
-  val inv : t -> t
-  val empty : t
-end
+type 'a aexpr = {
+  vars : 'a;  (* int array | int Ptmap.t *)
+  const : int;
+}
 
-module Make(A : Abelian) = struct
+let count = 
+  Array.fold_left begin fun a x -> 
+    Ptmap.update x begin function
+      | Some c -> Some (c + 1)
+      | None -> Some 0
+    end a
+  end Ptmap.empty
 
-  type uabel = _uabel uref
-  and _uabel = 
-    | Abel of A.t
-    | AVar of int
-  
-  type 'a aexpr = {
-    vars : 'a;  (* A.t array | A.t Ptmap.t *)
-    const : A.t;
-  }
-
-  let count = 
-    Array.fold_left begin fun a x -> 
-      Ptmap.update x begin function
-        | Some c -> Some (c + 1)
-        | None -> Some 0
-      end a
-    end Ptmap.empty
-
-  let one_side l r = {
-    vars = Ptmap.fold begin fun k v a ->
-      Ptmap.update k begin function
-        | Some v' -> Some (A.add v' (A.inv v))
-        | None -> Some (A.inv v)
-      end a
-    end r.vars l.vars;
-    const = A.add l.const (A.inv r.const)
-  }
-
-end
+let one_side l r = {
+  vars = Ptmap.fold begin fun k v a ->
+    Ptmap.update k begin function
+      | Some v' -> Some (v' - v)
+      | None -> Some (-v)
+    end a
+  end r.vars l.vars;
+  const = l.const - r.const
+}
