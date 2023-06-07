c2d16b1df7f5b8b9a461d0dc3a4b9a51eefae2a1
UberPyro <uberpyro181@gmail.com>
2023-06-07 14:57:34 -0400

    Scaffold out types for typechecking

---
+++ lib/type/types.ml
@@ -0,0 +1,19 @@
+open! Batteries
+open Uref
+
+type tlit = Int | String
+type tcon = Quote | List
+
+type v = v_contents uref
+and v_contents = 
+  | TLit of tlit
+  | TCon of tcon * dc * dc
+  | TMeta of int
+
+and s = v Ull.t
+and ds = s * s
+
+and c = ds Ull.t
+and dc = c * c
+
+
