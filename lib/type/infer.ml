6ef8d8ed814ff240fbf1962a03c0721119c3945e
UberPyro <uberpyro181@gmail.com>
2023-06-07 16:00:20 -0400

    Start inference

--- lib/type/infer.ml
+++ lib/type/infer.ml
@@ -1,3 +1,8 @@
 open! Batteries
 
-(* let rec infer =  *)
+(* let rec infer ((node, sp, c1, c2) : Parse.Ast.expr) = match node with
+  | Bop (e1, Aop _, e2) -> 
+
+    failwith "todo"
+  
+  | _ -> failwith "todo" *)
