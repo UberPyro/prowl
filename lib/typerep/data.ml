113c115e39378f587e108c2d54c2977524cb76cc
UberPyro <uberpyro181@gmail.com>
2023-06-20 11:46:07 -0400

    Redesign again using laziness

--- lib/typerep/data.ml
+++ lib/typerep/data.ml
@@ -4,14 +4,14 @@ open Uref
 type 'a bref = 'a _bref uref
 and 'a _bref = {
   dat : 'a;
-  back : fn;
+  back : fn Lazy.t;
 }
 
-and fn = <
+and fn = {
   dec : costack array;
   bot : costack;
   inc : costack array;
->
+}
 
 and 'a seq = 'a _seq bref
 and 'a _seq = 
@@ -36,12 +36,40 @@ and con =
   | Quote
   | List
 
-let mk_null_fn () = object (self)
-  method private mk = uref {
-    dat = Null;
-    back = self;
-  }
-  method dec = [|self#mk|]
-  method bot = self#mk
-  method inc = [|self#mk|]
-end
+let mk_null_fn () = 
+  let rec lazy_null_fn = 
+    let mk_null_costack () = 
+      uref {
+        dat = Null;
+        back = lazy_null_fn;
+      } in
+    (* each is only computed once *)
+    lazy {
+      dec = [|mk_null_costack ()|];
+      bot = mk_null_costack ();
+      inc = [|mk_null_costack ()|];
+    } in
+  Lazy.force lazy_null_fn
+
+let mk_free_fn () = 
+  let rec lazy_free_fn = 
+    let mk_free_costack () = 
+      uref {
+        dat = Push (
+          uref {
+            dat = Next (unique ());
+            back = lazy_free_fn;
+          }, 
+          uref {
+            dat = Next (unique ());
+            back = lazy_free_fn
+          }
+        );
+        back = lazy_free_fn;
+      } in
+    lazy {
+      dec = [|mk_free_costack ()|];
+      bot = mk_free_costack ();
+      inc = [|mk_free_costack ()|];
+    } in
+  Lazy.force lazy_free_fn
