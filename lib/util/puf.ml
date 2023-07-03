b47be7407496112bd5120ba559e062be7a1fd09d
UberPyro <uberpyro181@gmail.com>
2023-07-02 20:02:41 -0400

    New persistent union-find

--- lib/util/puf.ml
+++ lib/util/puf.ml
@@ -1,45 +1,28 @@
 open! Batteries
 open Ptmap
-open Tuple3
-
-exception MergeError
+open Either
+open Tuple2
 
 let modify k f = update k @@ function
   | Some x -> Some (f x)
   | None -> raise @@ Invalid_argument "ptmap modify"
 
-let move k1 k2 t = match find_opt k1 t with
-  | None -> t
-  | s -> t |> begin update k2 @@ function
-    | Some _ -> raise MergeError
-    | None -> s
-  end |> remove k1
+(* roots produce 'a, children produce int *)
+type 'a t = ('a, int) Either.t Ptmap.t * int Ptmap.t
 
-type 'a t = int Ptmap.t * int Ptmap.t * 'a Ptmap.t
+let rec search x t = match find x @@ fst t with
+  | Left v -> x, v
+  | Right i -> search i t
 
-let parent x = find x % first
-let rank x = find x % second
+let merge sel x y t = 
+  let ix, vx = search x t
+  and iy, vy = search y t in
+  if ix = iy then t
+  else let new_rep = Left (sel vx vy) in
+    match Stdlib.compare (find x @@ snd t) (find y @@ snd t) with
+    | 1 -> map1 (add iy (Right ix) % add ix new_rep) t
+    | -1 -> map1 (add ix (Right iy) % add iy new_rep) t
+    | _ -> map (add ix (Right iy) % add iy new_rep) (modify iy succ) t
 
-let rec search x t = 
-  let p = parent x t in
-  if p = x then x
-  else search p t
-
-let merge x y t = 
-  let rx = search x t
-  and ry = search y t in
-  if rx = ry then t
-  else match Stdlib.compare (rank x t) (rank y t) with
-  | 1 -> map (add ry rx) Fun.id (move ry rx) t
-  | -1 -> map (add rx ry) Fun.id (move rx ry) t
-  | _ -> map (add rx ry) (modify ry succ) (move rx ry) t
-
-let empty = empty, empty, empty
-let add x = map (add x x) (add x 0) Fun.id
-
-let get x t = third t |> Ptmap.find_opt (search x t)
-let set x y t = 
-  t |> map3 @@ Ptmap.update (search x t) begin function
-    | Some _ -> raise MergeError
-    | None -> Some y
-  end
+let empty = empty, empty
+let add x v = map (add x v) (add x 0)
--- lib/util/puf.mli
+++ lib/util/puf.mli
@@ -2,8 +2,6 @@ open! Batteries
 
 type 'a t
 
-val merge : int -> int -> 'a t -> 'a t
+val merge : ('a -> 'a -> 'a) -> int -> int -> 'a t -> 'a t
 val empty : 'a t
-val add : int -> 'a t -> 'a t
-val get : int -> 'a t -> 'a option
-val set : int -> 'a -> 'a t -> 'a t
+val add : int -> 'a-> 'a t -> 'a t
