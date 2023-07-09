f85bfc2c49a66813d3fad07fa2516d519395c301
UberPyro <uberpyro181@gmail.com>
2023-07-09 19:04:34 -0400

    Implement type infernece for all dataflow combinators

--- lib/semantics/infer.ml
+++ lib/semantics/infer.ml
@@ -9,6 +9,42 @@ open Tast
 open Util
 open Ull
 
+let inspect i o i_final o_final = 
+  let arg, argcos = usplit i in
+  let res, rescos = usplit o in
+  match argcos, rescos with
+  | None, _ | _, None -> failwith "Void sequence passed to dataflow"
+  | Some ka, Some kr when ka = kr -> 
+    ujoin i_final arg, ujoin o_final res
+  | _ -> failwith "Insufficiently specific data passed to dataflow"
+
+let inspect_biased disj conj disj_final = 
+  let disj_list, disj_key = usplit disj in
+  let _, conj_key = usplit conj in
+  match disj_key, conj_key with
+  | None, _ | _, None -> failwith "Void seq passed to dataflow [biased]"
+  | Some kd, Some kc when kd = kc -> ujoin disj_final disj_list
+  | _ -> failwith "Insufficiently specific data passed to dataflow [biased]"
+
+let inspect_nested i o i_final o_final = 
+  let arg, argcos = usplit i in
+  let res, rescos = usplit o in
+  match argcos, rescos with
+  | None, _ | _, None -> failwith "Void sequence passed to dataflow"
+  | Some ka, Some kr when ka = kr -> 
+    map_hd (fun x -> ujoin x arg) i_final, 
+    map_hd (fun x -> ujoin x res) o_final
+  | _ -> failwith "Insufficiently specific data passed to dataflow"
+
+let inspect_nested_biased disj conj disj_final = 
+  let disj_list, disj_key = usplit disj in
+  let _, conj_key = usplit conj in
+  match disj_key, conj_key with
+  | None, _ | _, None -> failwith "Void seq passed to dataflow [biased]"
+  | Some kd, Some kc when kd = kc -> 
+    map_hd (fun x -> ujoin x disj_list) disj_final
+  | _ -> failwith "Insufficiently specific data passed to dataflow [biased]"
+
 let rec infer ctx uctx (ast, _sp, (i0, o0)) = match ast with
   | Bop ((_, _, (i1, o1) as left), Aop _, (_, _, (i2, o2) as right)) -> 
     infer ctx uctx left;
@@ -112,21 +148,25 @@ let rec infer ctx uctx (ast, _sp, (i0, o0)) = match ast with
     o1 =?= o0;
     o2 =?= o0
   
+  | Dop ((_, _, (i1, o1) as left), Tensor, (_, _, (i2, o2) as right)) -> 
+    infer ctx uctx left;
+    infer ctx uctx right;
+    let i2s, o2s = Tuple2.mapn ((|>) ()) (ufresh, ufresh) in
+    i2s >>: unil () =?= i2;
+    o2s >>: unil () =?= o2;
+    let i, o = inspect_nested i2s o2s i1 o1 in
+    i =?= i0;
+    o =?= o0
+  
+  | Dop ((_, _, (i1, o1) as left), Fork, (_, _, (i2, o2) as right)) -> 
+    infer ctx uctx left;
+    infer ctx uctx right;
+    let i2s, o2s = Tuple2.mapn ((|>) ()) (ufresh, ufresh) in
+    i2s >>: unil () =?= i2;
+    o2s >>: unil () =?= o2;
+    let o = inspect_nested_biased i2s o2s o1 in
+    i1 =?= i0;
+    i2 =?= i0;
+    o =?= o0
+  
   | _ -> failwith "todo"
-
-and inspect i o i_final o_final = 
-  let arg, argcos = usplit i in
-  let res, rescos = usplit o in
-  match argcos, rescos with
-  | None, _ | _, None -> failwith "Void sequence passed to dataflow"
-  | Some ka, Some kr when ka = kr -> 
-    ujoin i_final arg, ujoin o_final res
-  | _ -> failwith "Insufficiently specific data passed to dataflow"
-
-and inspect_biased disj conj disj_final = 
-  let disj_list, disj_key = usplit disj in
-  let _, conj_key = usplit conj in
-  match disj_key, conj_key with
-  | None, _ | _, None -> failwith "Void seq passed to dataflow [biased]"
-  | Some kd, Some kc when kd = kc -> ujoin disj_final disj_list
-  | _ -> failwith "Insufficiently specific data passed to dataflow [biased]"
